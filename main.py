de075cffa9644267fc8b0e1dfa49c0f8fb0471cd
Tom Raybould <tr17544@bristol.ac.uk>
2021-05-03 14:12:03 +0100

    fixed rotation error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import ffmpeg
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 def check_file_exists(file):
     path = str(Path().absolute()) + '/' + file
@@ -11,9 +12,17 @@ def check_file_exists(file):
 
 def check_rotation(video_file_location):
     rotate_code = None
-    meta_data = ffmpeg.probe(video_file_location)
-    rotation = int(meta_data.get('streams', [dict(tags=dict())])[0].get('tags', dict()).get('rotate', 0))
-            
+    probe = ffmpeg.probe(video_file_location)
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    rotate = int(video_stream.get('side_data_list', [dict(tags=dict())])[0].get('rotation', 0))
+    
+    rotate_code = None
+    if (rotate == 90) or (rotate == -270):
+        rotate_code = cv2.ROTATE_90_CLOCKWISE
+    elif (rotate == -180) or (rotate == 180):
+        rotate_code = cv2.ROTATE_180
+
+    return rotate_code
 
 def decompose_video(video_file_location):
     # Make a folder to store frames captured from the video
@@ -29,16 +38,34 @@ def decompose_video(video_file_location):
         print("File does not exist or cannot be found")
         return
     
-    # rotate_code = check_rotation(video_file_location)
+    rotate_code = check_rotation(video_file_location)
 
     success, frame = video_capture_obj.read()
     count = 0
     while success:
+        if rotate_code is not None:
+            frame = cv2.rotate(frame, rotate_code)
         cv2.imwrite("tmp_frames/frame_%d.jpg" % count, frame)
         success, frame = video_capture_obj.read()
+
         count += 1
     
     return count
+
+def process_frames(frame_count):
+    for current_frame in range(0, 1):
+        path = str(Path().absolute()) + '/tmp_frames/frame_' + str(current_frame) + '.jpg'
+        frame = cv2.imread(path)
+        plt.imshow(frame)
+        plt.show()
+        get_pose_estimation(frame)
+
+
+def get_pose_estimation(frame):
+    frame_width = frame.shape[0]
+    frame_height = frame.shape[1]
+
+    print(frame_width, frame_height)
 
 if __name__ == '__main__':
     
@@ -49,4 +76,5 @@ if __name__ == '__main__':
 
     video_file_location=args.input
 
-    count = decompose_video(video_file_location) 
+    frame_count = decompose_video(video_file_location) 
+    process_frames(frame_count)
